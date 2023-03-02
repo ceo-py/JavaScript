f8a02f1c423828f3e3a23468b93201916b732bbc
ceo-py <sfmf@abv.bg>
2023-03-02 17:01:39 +0200

    init

---
+++ Programming Basics/Conditional Statements - Exercise/7_shopping.js
@@ -0,0 +1,30 @@
+function enoughBudget(array) {
+    const [budget, videoCards, cpu, ram] = array.map(x => Number(x))
+
+    const videoCardPrice = 250
+
+    const videoCardSum = videoCards * videoCardPrice
+    const cpuPrice = videoCardSum * 0.35
+    const ramPrice = videoCardSum * 0.10
+
+    const cpuSum = cpuPrice * cpu
+    const ramSum = ramPrice * ram
+
+    let totalSum = videoCardSum + cpuSum + ramSum
+
+
+    if (videoCards > cpu) {
+        totalSum *= 0.85
+    }
+
+    const moneyLeft = Math.abs(totalSum - budget).toFixed(2)
+
+
+    if (totalSum <= budget) {
+        console.log(`You have ${moneyLeft} leva left!`)
+    } else {
+        console.log(`Not enough money! You need ${moneyLeft} leva more!`)
+    }
+}
+
+enoughBudget(["900", "2", "1", "3"])
