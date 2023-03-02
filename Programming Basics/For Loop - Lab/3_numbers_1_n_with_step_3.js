f3b77e5df15eddf44703217e9515e3914a996fe6
ceo-py <sfmf@abv.bg>
2023-03-02 22:43:13 +0200

    init

---
+++ Programming Basics/For Loop - Lab/3_numbers_1_n_with_step_3.js
@@ -0,0 +1,10 @@
+function printNumberSkippingEvery3rd(number) {
+    number = Number(number)
+
+    for (let i = 1; i <= number; i += 3) {
+        console.log(i)
+    }
+}
+
+printNumberSkippingEvery3rd(["10"])
+
