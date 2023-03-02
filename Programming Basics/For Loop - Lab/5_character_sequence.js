65876d5a20b67c6dcfdd77fba073ab29d2b54e68
ceo-py <sfmf@abv.bg>
2023-03-02 22:49:54 +0200

    init

---
+++ Programming Basics/For Loop - Lab/5_character_sequence.js
@@ -0,0 +1,9 @@
+function printSymbolsFromString(string) {
+    string = string[0]
+
+    for (let i = 0; i < string.length; i++) {
+        console.log(string[i])
+    }
+}
+
+printSymbolsFromString(["softuni"])
