f24824187e8f4b1be28763d413bd1ae5d3c164f7
ceo-py <sfmf@abv.bg>
2023-03-02 16:40:34 +0200

    init

---
+++ Programming Basics/Conditional Statements - Exercise/5_godzilla_vs_kong.js
@@ -0,0 +1,22 @@
+function enoughMoneyForMovie(array) {
+    let [budget, extra, costumePricePerExtra] = array.map(x => Number(x))
+
+    const decoration = budget * 0.10
+
+    if (extra > 150) {
+        costumePricePerExtra *= 0.90
+    }
+
+    const totalSum = (extra * costumePricePerExtra) + decoration
+    const moneyLeft = Math.abs(budget - totalSum).toFixed(2)
+
+    if (budget >= totalSum) {
+        console.log(`Action!\nWingard starts filming with ${moneyLeft} leva left.`)
+    } else {
+        console.log(`Not enough money!\nWingard needs ${moneyLeft} leva more.`)
+    }
+}
+
+enoughMoneyForMovie(["20000",
+"120",
+"55.5"])
