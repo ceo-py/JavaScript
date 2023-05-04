a8cb1d1c8b625f50d60363cb36651d89aa9268c9
ceo-py <sfmf@abv.bg>
2023-05-04 20:41:56 +0300

    ref

---
+++ Programming Basics/Conditional Statements Advanced - More Exercises/1_match_tickets.js
@@ -0,0 +1,31 @@
+function matchTickets(list) {
+    const [budget, category, peopleNumber] = list.map(x => isNaN(x) ? x : Number(x))
+    let [discount, ticketPrice, total] = [null, null, null]
+
+    if (peopleNumber >= 1 && peopleNumber < 5) {
+        discount = 0.25
+    } else if (peopleNumber < 10) {
+        discount = 0.40
+    } else if (peopleNumber < 25) {
+        discount = 0.50
+    } else if (peopleNumber < 50) {
+        discount = 0.60
+    } else if (peopleNumber >= 50) {
+        discount = 0.75
+    }
+
+    // if (category === 'VIP') {
+    //     ticketPrice = 499.99
+    // } else if (category === 'Normal') {
+    //     ticketPrice = 249.99
+    // }
+
+    ticketPrice = category === 'VIP' ? 499.99 : 249.99
+
+    total = (budget * discount) - ticketPrice * peopleNumber
+
+    console.log(total >= 0 ? `Yes! You have ${total.toFixed(2)} leva left.` :
+                             `Not enough money! You need ${Math.abs(total).toFixed(2)} leva.`)
+}
+
+matchTickets(['30000', 'VIP', '49'])
