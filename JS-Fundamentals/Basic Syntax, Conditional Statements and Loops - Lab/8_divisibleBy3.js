75fae40a79ab47e7d5a0f819302173d50d20feaa
ceo-py <sfmf@abv.bg>
2023-11-04 14:43:49 +0200

    another solution

---
+++ JS-Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/8_divisibleBy3.js
@@ -0,0 +1,17 @@
+function solve() {
+    for (let x = 3; x <= 100; x+=3) {
+        console.log(x)
+    }
+}
+
+
+// function solve() {
+//     let i = 1
+//
+//     while ( i < 101) {
+//         if (i % 3 === 0) console.log(i)
+//         i += 1
+//     }
+// }
+
+solve()
