6cbc4043e4433412c74ddb5709f22255c76f57fc
ceo-py <sfmf@abv.bg>
2023-11-04 14:49:12 +0200

    another solution

---
+++ JS-Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/12_numbers_from_m_to_n.js
@@ -0,0 +1,15 @@
+function solve(start, end) {
+    while (start >= end) {
+        console.log(start)
+        start -= 1
+    }
+}
+
+// function solve (start, end) {
+//
+//     for (let i=start; i>=end; i--){
+//         console.log(i)
+//     }
+// }
+
+solve(6, 2)
