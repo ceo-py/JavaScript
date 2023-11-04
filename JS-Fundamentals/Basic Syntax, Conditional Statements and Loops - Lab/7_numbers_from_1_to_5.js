bae7c99da78f1af7f46b6fe429ab6c637b1327b4
ceo-py <sfmf@abv.bg>
2023-11-04 14:39:19 +0200

    another solution

---
+++ JS-Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/7_numbers_from_1_to_5.js
@@ -0,0 +1,16 @@
+function solve() {
+    let i = 1
+
+    while (i < 6) {
+        console.log(i)
+        i += 1
+    }
+}
+
+// function solve () {
+//
+//     for (let i=1; i<6; i++){
+//         console.log(i)
+//     }
+// }
+
