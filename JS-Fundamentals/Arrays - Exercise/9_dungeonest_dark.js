48b4c39f6d9c6a7a319e477d6c8a02a8701609d6
ceo-py <sfmf@abv.bg>
2023-11-09 15:48:35 +0200

    another solution

---
+++ JS-Fundamentals/Arrays - Exercise/9_dungeonest_dark.js
@@ -0,0 +1,29 @@
+function dungeonestDark(rooms) {
+    let [health, coins, alive] = [100, 0, true]
+    for (const [roomNumber, room] of rooms[0].split('|').entries()) {
+        let [value, number] = room.split(' ').map(x => isNaN(x) ? x : Number(x))
+        if (value === 'potion') {
+            if (number + health > 100) number = 100 - health
+            health += number
+            console.log(`You healed for ${number} hp.`)
+            console.log(`Current health: ${health} hp.`)
+        } else if (value === 'chest') {
+            coins += number
+            console.log(`You found ${number} coins.`)
+        } else {
+            health -= number
+            if (health <= 0) {
+                console.log(`You died! Killed by ${value}.`)
+                console.log(`Best room: ${roomNumber + 1}`)
+                alive = false
+                break
+            }
+            console.log(`You slayed ${value}.`)
+        }
+    }
+    if (alive) {
+        console.log(`You've made it!\nCoins: ${coins}\nHealth: ${health}`)
+    }
+}
+
+dungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
