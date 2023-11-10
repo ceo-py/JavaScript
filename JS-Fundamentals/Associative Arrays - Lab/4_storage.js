6181cf2653d79ee0677274a0ba9d329ed50a1c49
ceo-py <sfmf@abv.bg>
2023-11-11 00:24:20 +0200

    another solution

---
+++ JS-Fundamentals/Associative Arrays - Lab/4_storage.js
@@ -0,0 +1,31 @@
+function storage(list) {
+    const items = []
+    list.forEach(x => {
+        const [item, value] = x.split(' ').map(x => isNaN(x)? x: Number(x))
+        const foundItem = items.filter(x => x.item === item )[0]
+        if (!foundItem) items.push({item, value})
+        else foundItem.value += value
+    })
+    items.forEach(x => console.log(`${x.item} -> ${x.value}`))
+}
+
+
+
+// function storage(list) {
+//   const itemsMap = new Map()
+//
+//   list.forEach(entry => {
+//     const [item, value] = entry.split(' ').map(x => (isNaN(x) ? x : Number(x)))
+//     itemsMap.set(item, (itemsMap.get(item) || 0) + value)
+//   })
+//
+//   itemsMap.forEach((value, item) => {
+//     console.log(`${item} -> ${value}`)
+//   })
+// }
+
+storage(['tomatoes 10',
+'coffee 5',
+'olives 100',
+'coffee 40']
+)
