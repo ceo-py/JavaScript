1e62d5731e25d2eade53b8c483f658d30152899d
ceo-py <sfmf@abv.bg>
2023-11-10 20:16:00 +0200

    another solution

---
+++ JS-Fundamentals/Associative Arrays - Exercise/8_travel_time.js
@@ -0,0 +1,14 @@
+function travelTime(input) {
+    const destinations = {}
+    input.forEach(x => {
+        const [country, town, value] = x.split(' > ').map(x => isNaN(x) ? x : Number(x))
+        if (!destinations.hasOwnProperty(country)) destinations[country] = {[town]: value}
+        else destinations[country][town] = Math.min(destinations[country][town]? destinations[country][town]: value, value)
+    })
+    Object.keys(destinations).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })).forEach(x => {
+        const towns = Object.entries(destinations[x]).sort((a, b) => a[1] - b[1]).map(t => `${t[0]} -> ${t[1]}`)
+        console.log(`${x} -> ${towns.join(' ')}`)
+    })
+}
+
+travelTime(["Bulgaria > Sofia > 500", "Bulgaria > Sopot > 800", "France > Paris > 2000", "Albania > Tirana > 1000", "Bulgaria > Sofia > 200"])
