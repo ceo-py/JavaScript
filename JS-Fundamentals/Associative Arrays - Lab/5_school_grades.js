c03b52c627d7d0247dd2c8f58f05114ea6057a96
ceo-py <sfmf@abv.bg>
2023-11-11 00:37:45 +0200

    another solution

---
+++ JS-Fundamentals/Associative Arrays - Lab/5_school_grades.js
@@ -0,0 +1,14 @@
+function schoolGrades(list) {
+    const students = []
+
+    list.forEach(x => {
+        const [name, ...grades] = x.split(' ')
+        const foundStudent = students.filter(x => x.name === name)[0]
+        if (!foundStudent) students.push({name, grades: [...grades]})
+        else foundStudent.grades.push(...grades)
+    })
+    students.sort((a, b) => a.name.localeCompare(b.name)).forEach(x =>
+        console.log(`${x.name}: ${(eval(x.grades.join('+'))/x.grades.length).toFixed(2)}`))
+}
+
+schoolGrades(['Lilly 4 6 6 5', 'Tim 5 6', 'Tammy 2 4 3', 'Tim 6 6'])
