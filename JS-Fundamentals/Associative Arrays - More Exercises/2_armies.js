c93f954d7cce83b2f5427f7b850b1b152a29a590
ceo-py <sfmf@abv.bg>
2023-11-11 00:50:29 +0200

    another solution

---
+++ JS-Fundamentals/Associative Arrays - More Exercises/2_armies.js
@@ -0,0 +1,48 @@
+function solve(list) {
+    output = {}
+
+    list.forEach(item => {
+        if (item.includes('defeated')) {
+            item = item.replace(' defeated', '')
+            delete output[item]
+
+        } else if (item.includes('arrives') && !output.hasOwnProperty(item.replace(' arrives', ''))) {
+            item = item.replace(' arrives', '')
+            output[item] = {}
+            output[item].armys = []
+            output[item].totalArmy = 0
+
+        } else if (item.includes('+')) {
+            [armyName, count] = item.split(' + ')
+            for (key in output) {
+                output[key].armys.forEach(item => {
+
+                    if (item.name === armyName) {
+                        count = parseInt(count)
+                        item.count += count
+                        output[key].totalArmy += count
+                    }
+                })
+            }
+
+        } else {
+            [leader, ...armyInfo] = item.split(': ')
+            let [armyName, armyCount] = armyInfo[0].split(', ')
+
+            if (output.hasOwnProperty(leader)) {
+                armyCount = parseInt(armyCount)
+                output[leader].armys.push({name: armyName, count: armyCount})
+                output[leader].totalArmy += armyCount
+            }
+        }
+    })
+
+    for (const [name, army] of Object.entries(output)
+        .sort(([, a], [, b]) => b.totalArmy - a.totalArmy)) {
+        console.log(`${name}: ${army.totalArmy}`)
+
+        for (const {name, count} of army.armys.sort((a, b) => b.count - a.count)) {
+            console.log(`>>> ${name} - ${count}`)
+        }
+    }
+}
