736db4a6eac4561a1e68db28333a3b91b1f3f590
ceo-py <sfmf@abv.bg>
2023-11-11 00:47:46 +0200

    another solution

---
+++ JS-Fundamentals/Associative Arrays - More Exercises/1_garage.js
@@ -0,0 +1,24 @@
+function garage(list) {
+        const parking = {}
+
+    for (const elem of list) {
+        const [garage, ...car] = elem.split(' - ')
+
+        if (garage in parking) {
+            parking[garage].push(...car)
+            continue;
+        }
+        parking[garage] = car
+
+    }
+
+    for (const entry of Object.entries(parking)) {
+
+        console.log(`Garage № ${entry[0]}`)
+        for (const elem of entry[1]) {
+            console.log(`--- ${elem.replace(/\:/g, ' -')}`);
+        }
+    }
+}
+
+garage([])
