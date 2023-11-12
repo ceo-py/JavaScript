c03f63dd73d572d870c8673030a5afa2dd89bed0
ceo-py <sfmf@abv.bg>
2023-11-12 12:32:46 +0200

    another solution

---
+++ JS-Fundamentals/Text Processing - Exercise/1_reveal_words.js
@@ -0,0 +1,12 @@
+function solve(words, string){
+    words = words.split(', ')
+    string = string.split(' ')
+    for (let i=0; i < string.length; i++){
+        for (const word of words){
+            if (string[i][0] === '*' && string[i].length === word.length){
+                string[i] = word
+            }
+        }
+    }
+    console.log(string.join(' '))
+}
