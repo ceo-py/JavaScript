4c4828115697b8d1d9e9183110cc0c6eeff748bd
ceo-py <sfmf@abv.bg>
2023-11-12 12:34:41 +0200

    another solution

---
+++ JS-Fundamentals/Text Processing - Exercise/2_modern_times_of_hashtag.js
@@ -0,0 +1,16 @@
+function solve(string) {
+    string.split(' ').forEach(w => w.match(/#[A-Za-z]/) ? console.log(w.slice(1, w.length)) : null)
+}
+
+
+// function solve(string){
+//     string = string.split(' ')
+//     function onlyLettersAndNumbers(str) {
+//   return Boolean(str.match(/#[A-Za-z]/));
+//     }
+//     for (const word of string){
+//         if (onlyLettersAndNumbers(word)){
+//             console.log(word.slice(1, word.length))
+//         }
+//     }
+// }
