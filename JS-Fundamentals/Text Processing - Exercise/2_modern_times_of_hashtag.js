e28bdb736f3d02a5d387e85bd9223a75826073d5
ceo-py <sfmf@abv.bg>
2023-11-28 19:28:33 +0200

    another solution

--- JS-Fundamentals/Text Processing - Exercise/2_modern_times_of_hashtag.js
+++ JS-Fundamentals/Text Processing - Exercise/2_modern_times_of_hashtag.js
@@ -1,6 +1,16 @@
 function solve(string) {
-    string.split(' ').forEach(w => w.match(/#[A-Za-z]/) ? console.log(w.slice(1, w.length)) : null)
+    string.split(' ').forEach(w => w.match(/#[A-Za-z]{2,}/) && console.log(w.slice(1)))
 }
+
+
+// function solve(string) {
+//     string.split(' ').forEach(w => w.match(/#[A-Za-z]{2,}/) ? console.log(w.slice(1, w.length)) : null)
+// }
+
+
+// function solve(string) {
+//     string.split(' ').filter(word => word[0] === "#" && Array.from(word.slice(1)).every(c => (c.toUpperCase() >= 'A' && c.toUpperCase() <= 'Z'))).forEach(w => w.length > 1? console.log(w.slice(1)): null)
+// }
 
 
 // function solve(string){
@@ -14,3 +24,5 @@ function solve(string) {
 //         }
 //     }
 // }
+
+solve("Nowadays everyone uses # to tag a #special word in #socialMedia")
