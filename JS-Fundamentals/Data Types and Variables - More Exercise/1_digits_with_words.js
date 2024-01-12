51be57681ce4f8e73b8ee495876df387f015a5fd
ceo-py <sfmf@abv.bg>
2024-01-12 23:42:54 +0200

    another solution

---
+++ JS-Fundamentals/Data Types and Variables - More Exercise/1_digits_with_words.js
@@ -0,0 +1,8 @@
+function digitsWithWords(stringNumber) {
+    const number = {
+        'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
+    }
+    console.log(number[stringNumber])
+}
+
+digitsWithWords('nine')
