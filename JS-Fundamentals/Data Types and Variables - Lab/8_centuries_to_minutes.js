134080857c1e1663183a15c9d66e66c1f2627977
ceo-py <sfmf@abv.bg>
2024-01-12 22:54:51 +0200

    another solution

---
+++ JS-Fundamentals/Data Types and Variables - Lab/8_centuries_to_minutes.js
@@ -0,0 +1,9 @@
+function centuriesToMinutes(centuries) {
+    const years = centuries * 100
+    const days = Math.floor(years * 365.2422)
+    const hours = days * 24
+    const minutes = hours * 60
+    console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`)
+}
+
+centuriesToMinutes(1)
