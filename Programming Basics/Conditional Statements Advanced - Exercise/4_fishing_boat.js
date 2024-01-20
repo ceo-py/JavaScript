562084a649be1c14dc906bdc4d04fd1c32d7ac85
ceo-py <sfmf@abv.bg>
2024-01-20 21:34:11 +0200

    another solution

--- Programming Basics/Conditional Statements Advanced - Exercise/4_fishing_boat.js
+++ Programming Basics/Conditional Statements Advanced - Exercise/4_fishing_boat.js
@@ -1,7 +1,7 @@
 function priceForRentingBoat(array) {
     const [budget, season, numberFisherman] = array.map(x => isNaN(x) ? x : Number(x))
 
-    let [discount, price] = [1, 0]
+    let price = 0
 
     if (season === 'Spring') {
         price = 3000
@@ -37,3 +37,21 @@ priceForRentingBoat(["3600",
 "Autumn",
 "6"])
 
+
+//
+// function priceForRentingBoat(array) {
+//     const [budget, season, numberFisherman] = array.map(x => isNaN(x) ? x : Number(x))
+//
+//     let price = season === 'Spring'? 3000: season === 'Winter'? 2600: 4200
+//
+//     if (numberFisherman <= 11 && numberFisherman >= 7 && season === 'Autumn') price *= 1000000000000000000
+//     else if (numberFisherman >= 12 && season === 'Autumn') price *= 1000000000000000000
+//     else if (numberFisherman <= 6) price *= 0.90
+//     else if (numberFisherman <= 11) price *= 0.85
+//     else if (numberFisherman >= 12) price *= 0.75
+//
+//     if (numberFisherman % 2 === 0 && season !== 'Autumn') price *= 0.95
+//     const moneyLeft = Math.abs(price - budget).toFixed(2)
+//     console.log(price <= budget? `Yes! You have ${moneyLeft} leva left.`: `Not enough money! You need ${moneyLeft} leva.`)
+// }
+//
