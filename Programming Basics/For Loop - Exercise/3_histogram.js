8d371dc8abe798e40666fc0fdd6429493195f987
ceo-py <sfmf@abv.bg>
2024-01-23 19:09:03 +0200

    another solution

--- Programming Basics/For Loop - Exercise/3_histogram.js
+++ Programming Basics/For Loop - Exercise/3_histogram.js
@@ -32,3 +32,80 @@ numbersInRange(["3",
     "1",
     "2",
     "999"])
+
+
+
+
+
+
+//
+// function histogram(params) {
+//     const arrayLen = Number(params.shift()) // params[0]
+//     // let [p1, p2, p3, p4, p5] = [0, 0, 0, 0, 0]
+//     const result = [0, 0, 0, 0, 0]
+//
+//     for (const iterator of params) {
+//
+//         const element = Number(iterator)
+//         element < 200 ? result[0]++ :
+//             element < 400 ? result[1]++ :
+//                 element < 600 ? result[2]++ :
+//                     element < 800 ? result[3]++ :
+//                         element >= 800 ? result[4]++ : null
+
+        // if (element < 200) {
+        //     p1++
+        // } else if (element < 400) {
+        //     p2++
+        // } else if (element < 600) {
+        //     p3++
+        // } else if (element < 800) {
+        //     p4++
+        // } else if (element >= 800) {
+        //     p5++
+        // }
+    // }
+
+    // for (let index = 3; index <= arrayLen; index+=2) {
+    //     const element = Number(params[index])
+
+    //     if (element < 200) {
+    //         p1++
+    //     } else if (element < 400) {
+    //         p2++
+    //     } else if (element < 600) {
+    //         p3++
+    //     } else if (element < 800) {
+    //         p4++
+    //     } else if (element >= 800) {
+    //         p5++
+    //     }
+    // }
+    // arrayLen = p1 + p2 + p3 + p4 + p5
+    // for (const number of result) {
+    //     console.log(`${(number / arrayLen * 100).toFixed(2)}%`);
+    // }
+
+    // result.forEach(x => console.log(`${(x / arrayLen * 100).toFixed(2)}%`))
+
+
+    //     console.log(`${(p1 / arrayLen * 100).toFixed(2)}%`);
+    //     console.log(`${(p2 / arrayLen * 100).toFixed(2)}%`);
+    //     console.log(`${(p3 / arrayLen * 100).toFixed(2)}%`);
+    //     console.log(`${(p4 / arrayLen * 100).toFixed(2)}%`);
+    //     console.log(`${(p5 / arrayLen * 100).toFixed(2)}%`);
+// }
+
+// histogram(
+//     [
+//         "9",
+//         "367",
+//         "99",
+//         "200",
+//         "799",
+//         "999",
+//         "333",
+//         "555",
+//         "111",
+//         "9"
+//     ])
