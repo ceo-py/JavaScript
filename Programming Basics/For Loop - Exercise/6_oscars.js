9225e6af9591db0176ad22bca6ba95a7a2240d50
ceo-py <sfmf@abv.bg>
2024-01-24 19:01:21 +0200

    another solution

--- Programming Basics/For Loop - Exercise/6_oscars.js
+++ Programming Basics/For Loop - Exercise/6_oscars.js
@@ -26,3 +26,28 @@ function oscarWinners(array) {
 
 
 oscarWinners(["Zahari Baharov", "205", "4", "Johnny Depp", "45", "Will Smith", "29", "Jet Lee", "10", "Matthew Mcconaughey", "39"])
+
+//
+// function oscars(input) {
+//     let [actor, score, nominated, neededScore] = [input[0], Number(input[1]), false, 1250.5]
+//
+//     for (let index = 3; index < input.length; index += 2) {
+//         const juryName = input[index]
+//         const juryScore = Number(input[index + 1])
+//
+//         const currentScore = (juryName.length * juryScore) / 2
+//         score += currentScore
+//
+//         if (score > neededScore) {
+//             nominated = true
+//             break
+//         }
+//     }
+//
+//     if (nominated) {
+//         console.log(`Congratulations, ${actor} got a nominee for leading role with ${score.toFixed(1)}!`);
+//     } else {
+//         console.log(`Sorry, ${actor} you need ${(neededScore - score).toFixed(1)} more!`);
+//     }
+//
+// }
