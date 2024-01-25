6a650c91203716dad41cdc42f3896168d61dbeed
ceo-py <sfmf@abv.bg>
2024-01-25 22:22:17 +0200

    another solution

---
+++ JS-Fundamentals/Functions - More Exercise/2_number_modification.js
@@ -0,0 +1,18 @@
+function numberModification(numbers) {
+    numbers = numbers.toString()
+
+    function averageNumber(numbers) {
+        let sum = 0
+        for (let number of numbers) {
+            sum += parseInt(number)
+        }
+        return sum / numbers.length
+    }
+
+    while (averageNumber(numbers) < 5) {
+        numbers += '9'
+    }
+    console.log(numbers)
+}
+
+numberModification(101)
