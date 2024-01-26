e951d972613f89099085707f89e81ac308e3506f
ceo-py <sfmf@abv.bg>
2024-01-26 13:59:50 +0200

    another solution

---
+++ JS-Fundamentals/Arrays Advanced - Exercise/9_gladiator_inventory.js
@@ -0,0 +1,76 @@
+function gladiatorInventory(initialInput) {
+    const array = initialInput.shift().split(' ')
+    const commands = {
+        Buy: (equipment) => array.includes(equipment)? null: array.push(equipment),
+        Trash: (equipment) => !array.includes(equipment)? null: array.splice(array.indexOf(equipment), 1),
+        Repair: (equipment) => !array.includes(equipment)? null: array.push(array.splice(array.indexOf(equipment), 1)[0]),
+        Upgrade: (data) => {
+            const [equipment, upgrade] = data.split('-')
+            !array.includes(equipment) ? null : array.splice(array.indexOf(equipment) + 1, 0, `${equipment}:${upgrade}`)
+        },
+    }
+    initialInput.forEach(x => {
+        const [command, ...args] = x.split(' ')
+        commands[command](...args)
+    })
+    console.log(array.join(' '))
+}
+
+gladiatorInventory(['SWORD Shield Spear',
+'Buy Bag',
+'Trash Shield',
+'Repair Spear',
+'Upgrade SWORD-Steel'])
+
+
+
+// function manageInventory(commands) {
+//     let inventory = commands.shift().split(' ');
+//
+//     for (const command of commands) {
+//         const [action, ...args] = command.split(' ');
+//
+//         switch (action) {
+//             case 'Buy':
+//                 {
+//                     const equipment = args[0];
+//                     if (!inventory.includes(equipment)) {
+//                         inventory.push(equipment);
+//                     }
+//                 }
+//                 break;
+//             case 'Trash':
+//                 {
+//                     const equipment = args[0];
+//                     const index = inventory.indexOf(equipment);
+//                     if (index !== -1) {
+//                         inventory.splice(index, 1);
+//                     }
+//                 }
+//                 break;
+//             case 'Repair':
+//                 {
+//                     const equipment = args[0];
+//                     const index = inventory.indexOf(equipment);
+//                     if (index !== -1) {
+//                         inventory.splice(index, 1);
+//                         inventory.push(equipment);
+//                     }
+//                 }
+//                 break;
+//             case 'Upgrade':
+//                 {
+//                     const [equipment, upgrade] = args[0].split('-');
+//                     const index = inventory.indexOf(equipment);
+//                     if (index !== -1) {
+//                         inventory.splice(index + 1, 0, `${equipment}:${upgrade}`);
+//                     }
+//                 }
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+//
+//     console.log(inventory.join(' '));
+// }
