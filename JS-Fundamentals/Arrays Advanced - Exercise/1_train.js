a60eee99f25d39ac808992688fb54ba32e81f4fe
ceo-py <sfmf@abv.bg>
2024-01-26 11:30:29 +0200

    another solution

---
+++ JS-Fundamentals/Arrays Advanced - Exercise/1_train.js
@@ -0,0 +1,27 @@
+function train(array) {
+    const waggons = array.shift().split(' ').map(Number)
+    const capacity = array.shift()
+
+    array.forEach(x => {
+        if (x.includes('Add')) {
+            waggons.push(Number(x.split(' ')[1]))
+        } else {
+            x = Number(x)
+            for (const i in waggons) {
+                if (waggons[i] + x <= capacity) {
+                    waggons[i] += x
+                    break
+                }
+            }
+        }
+    })
+    console.log(waggons.join(' '))
+}
+
+train(['32 54 21 12 4 0 23',
+    '75',
+    'Add 10',
+    'Add 0',
+    '30',
+    '10',
+    '75'])
