c2806cf608d3f7b22193b7f50fd5e5a8454bdf4a
ceo-py <sfmf@abv.bg>
2024-01-26 23:36:18 +0200

    another solution

---
+++ JS-Fundamentals/Arrays Advanced - More Exercises/4_jans_notation.js
@@ -0,0 +1,44 @@
+function notation(arr) {
+    const stack = [];
+
+    for (const el of arr) {
+        if (typeof el === 'number') {
+            stack.push(el);
+        } else {
+            const operand2 = stack.pop();
+            const operand1 = stack.pop();
+
+            if (operand1 === undefined || operand2 === undefined) {
+                console.log('Error: not enough operands!');
+                return;
+            }
+
+            switch (el) {
+                case '+':
+                    stack.push(operand1 + operand2);
+                    break;
+                case '-':
+                    stack.push(operand1 - operand2);
+                    break;
+                case '*':
+                    stack.push(operand1 * operand2);
+                    break;
+                case '/':
+                    stack.push(operand1 / operand2);
+                    break;
+                default:
+                    console.log('Error: invalid operator!');
+                    return;
+            }
+        }
+    }
+
+    if (stack.length !== 1) {
+        console.log('Error: too many operands!');
+        return;
+    }
+
+    console.log(stack[0]);
+}
+
+notation([5, 3, 4, '*', '-']);
