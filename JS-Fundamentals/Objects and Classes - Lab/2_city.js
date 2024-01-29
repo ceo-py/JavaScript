fe487a337a5d7a9708b26ee2cc1286d64f2bef2a
ceo-py <sfmf@abv.bg>
2024-01-29 19:14:47 +0200

    another solution

---
+++ JS-Fundamentals/Objects and Classes - Lab/2_city.js
@@ -0,0 +1,13 @@
+function city(obj) {
+    for (const key in obj) {
+        console.log(`${key} -> ${obj[key]}`)
+    }
+}
+
+city({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+})
