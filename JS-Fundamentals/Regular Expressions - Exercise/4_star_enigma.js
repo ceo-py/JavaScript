b72488022fb8fba14a93dcdcca3b09348d1167a6
ceo-py <sfmf@abv.bg>
2024-01-31 19:04:55 +0200

    another solution

---
+++ JS-Fundamentals/Regular Expressions - Exercise/4_star_enigma.js
@@ -0,0 +1,30 @@
+function starEnigma(messages) {
+    let planets = {attacked: [], destroyed: []};
+    messages.shift();
+
+    for (const line of messages) {
+        const pattern = /@(?<planetName>[A-Za-z]+)[^@\-!:>]*?:(?<planetPopulation>\d+)[^@\-!:>]*?!(?<attackType>[AD])![^@\-!:>]*?->(?<soldierCount>\d+)/g;
+
+        let encryptionKey = 0;
+        if (line.match(/[star]/gi) !== null) {
+            encryptionKey = line.match(/[star]/gi).length;
+        }
+        const match = pattern.exec(line.split('').map(letter => letter.charCodeAt(0) - encryptionKey).map(code => String.fromCharCode(code)).join(''));
+        if (match === null) continue
+
+        const attackType = match.groups.attackType;
+        const planetName = match.groups.planetName;
+
+        attackType === 'A' ? planets.attacked.push(planetName) : attackType === 'D' ? planets.destroyed.push(planetName) : null
+    }
+    planets.attacked.sort((a, b) => a.localeCompare(b));
+    planets.destroyed.sort((a, b) => a.localeCompare(b));
+
+    console.log(`Attacked planets: ${planets.attacked.length}`);
+    planets.attacked.forEach(planet => console.log(`-> ${planet}`));
+
+    console.log(`Destroyed planets: ${planets.destroyed.length}`);
+    planets.destroyed.forEach(planet => console.log(`-> ${planet}`));
+}
+
+starEnigma(['2', 'STCDoghudd4=63333$D$0A53333', 'EHfsytsnhf?8555&I&2C9555SR'])
