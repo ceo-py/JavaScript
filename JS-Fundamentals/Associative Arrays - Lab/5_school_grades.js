33053c012609740abc37f18fd917a98094e396d0
ceo-py <sfmf@abv.bg>
2024-02-08 21:37:33 +0200

    another solution

--- JS-Fundamentals/Associative Arrays - Lab/5_school_grades.js
+++ JS-Fundamentals/Associative Arrays - Lab/5_school_grades.js
@@ -4,13 +4,24 @@ function schoolGrades(list) {
     list.forEach(x => {
         const [name, ...grades] = x.split(' ')
         const foundStudent = students.find(x => x.name === name)
-        // const foundStudent = students.filter(x => x.name === name)[0]
-
-        if (!foundStudent) students.push({name, grades: [...grades]})
-        else foundStudent.grades.push(...grades)
+        foundStudent ? foundStudent.grades.push(...grades) : students.push({name, grades: [...grades]})
     })
     students.sort((a, b) => a.name.localeCompare(b.name)).forEach(x =>
-        console.log(`${x.name}: ${(eval(x.grades.join('+'))/x.grades.length).toFixed(2)}`))
+        console.log(`${x.name}: ${(eval(x.grades.join('+')) / x.grades.length).toFixed(2)}`))
 }
+
+
+// function schoolGrades(list) {
+//     const students = []
+//
+//     list.forEach(x => {
+//         const [name, ...grades] = x.split(' ')
+//         const foundStudent = students.filter(x => x.name === name)[0]
+//         if (!foundStudent) students.push({name, grades: [...grades]})
+//         else foundStudent.grades.push(...grades)
+//     })
+//     students.sort((a, b) => a.name.localeCompare(b.name)).forEach(x =>
+//         console.log(`${x.name}: ${(eval(x.grades.join('+'))/x.grades.length).toFixed(2)}`))
+// }
 
 schoolGrades(['Lilly 4 6 6 5', 'Tim 5 6', 'Tammy 2 4 3', 'Tim 6 6'])
