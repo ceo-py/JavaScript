860bde04a2ea1f9c0fefb552187d9f50c56ec693
ceo-py <sfmf@abv.bg>
2024-02-09 18:12:47 +0200

    another solution

--- Programming Basics/For-Loop - More Exercises/7_football_league.js
+++ Programming Basics/For-Loop - More Exercises/7_football_league.js
@@ -19,6 +19,40 @@ function footballLeague(input) {
     console.log(`${(totalFans/capacityStadium*100).toFixed(2)}%`)
 }
 
+
+
+
+
+// function footballLeague(input) {
+//     const [capacityStadium, totalFans] = [input.shift(), input.shift()]
+//     const blocks = {A: 0, B: 0, V: 0, G: 0}
+//     input.forEach(b => blocks[b]++)
+//     Object.values(blocks).forEach(b => console.log(`${(b / totalFans * 100).toFixed(2)}%`))
+//     console.log(`${(totalFans / capacityStadium * 100).toFixed(2)}%`)
+// }
+
+
+
+
+
+// function footballLeague(input) {
+//     const [capacityStadium, totalFans] = [input.shift(), input.shift()]
+//     let blocks = [0, 0, 0, 0]
+//     for (const block of input) {
+//         if (block === 'A') {
+//             blocks[0]++
+//         } else if (block === 'B') {
+//             blocks[1]++
+//         } else if (block === 'V') {
+//             blocks[2]++
+//         } else if (block === 'G') {
+//             blocks[3]++
+//         }
+//     }
+//     blocks.forEach(b => console.log(`${(b/totalFans*100).toFixed(2)}%`) )
+//     console.log(`${(totalFans/capacityStadium*100).toFixed(2)}%`)
+// }
+
 footballLeague(['76',
     '10',
     'A',
