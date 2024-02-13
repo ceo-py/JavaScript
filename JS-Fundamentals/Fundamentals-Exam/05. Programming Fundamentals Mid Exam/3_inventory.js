bba6591a7525b22e44ac834fbea9da1d1be0e5a7
ceo-py <sfmf@abv.bg>
2024-02-13 22:06:31 +0200

    another solution

--- JS-Fundamentals/Fundamentals-Exam/05. Programming Fundamentals Mid Exam/3_inventory.js
+++ JS-Fundamentals/Fundamentals-Exam/05. Programming Fundamentals Mid Exam/3_inventory.js
@@ -15,3 +15,54 @@ function inventory(arr) {
     }
     console.log(collectedItems.join(', '))
 }
+
+
+
+//
+//
+// function main(sourceArr) {
+//     let inventory = sourceArr[0].split(", ");
+//     for (let i = 1; sourceArr[i] !== "Craft!"; i++) {
+//         let com = sourceArr[i].split(" - ")[0];
+//         let item = sourceArr[i].split(" - ")[1];
+//         switch (com) {
+//             case "Collect":
+//                 if (!inventory.includes(item)) {
+//                     inventory.push(item);
+//                 }
+//                 break;
+//             case "Drop":
+//                 if (inventory.indexOf(item) === -1) {
+//                     continue;
+//                 } else {
+//                     function removeItem() {
+//                         inventory.splice((inventory.indexOf(item)), 1);
+//                         if (inventory.indexOf(item) !== -1) {
+//                             removeItem();
+//                         }
+//                     }
+//
+//                     removeItem();
+//                 }
+//                 break;
+//             case "Combine Items":
+//                 let oldItem = item.split(":")[0];
+//                 let newItem = item.split(":")[1];
+//                 if (inventory.indexOf(oldItem) === -1) {
+//                     continue;
+//                 } else {
+//                     inventory.splice((inventory.indexOf(oldItem)) + 1, 0, newItem);
+//                 }
+//                 break;
+//             case "Renew":
+//                 if (inventory.indexOf(item) === -1) {
+//                     continue;
+//                 } else {
+//                     let deletedItem = inventory.splice((inventory.indexOf(item)), 1);
+//                     inventory.push(deletedItem[0]);
+//                 }
+//                 break;
+//         }
+//     }
+//     console.log(inventory.join(", "));
+// }
