382b451c45a9c49f75f7a4bbd74cf2241c6c118b
ceo-py <sfmf@abv.bg>
2024-02-18 18:12:47 +0200

    another solution

---
+++ Programming Basics/JS Basics - Exams/Programming Basics Online Pre - Exam - 17 and 18 February 2024/03_santas_holiday.js
@@ -0,0 +1,39 @@
+function santasHoliday(array) {
+    let [discount, price, onePerson, apartment, presidentApartment] = [1, 0, 18, 25, 35]
+    let [daysToStay, accommodateType, score] = array.map(x => isNaN(x) ? x : Number(x))
+
+    daysToStay -= 1
+
+    if (accommodateType === 'room for one person') {
+        price = onePerson * daysToStay
+    } else if (accommodateType === 'apartment') {
+        price = apartment * daysToStay
+
+        if (daysToStay <= 15) {
+            discount = 0.65
+        } else if (daysToStay < 10) {
+            discount = 0.70
+        } else if (daysToStay > 15) {
+            discount = 0.50
+        }
+    } else if (accommodateType === 'president apartment') {
+        price = presidentApartment * daysToStay
+
+        if (daysToStay <= 15) {
+            discount = 0.85
+        } else if (daysToStay < 10) {
+            discount = 0.90
+        } else if (daysToStay > 15) {
+            discount = 0.80
+        }
+    }
+
+    price *= discount
+
+    if (score === 'positive') {
+        price *= 1.25
+    } else if (score === 'negative') {
+        price *= 0.90
+    }
+    console.log(price.toFixed(2))
+}
