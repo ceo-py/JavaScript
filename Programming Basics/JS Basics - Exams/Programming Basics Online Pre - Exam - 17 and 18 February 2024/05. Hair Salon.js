5dde11880040cf468e5d3eccb8e53abe06601226
ceo-py <sfmf@abv.bg>
2024-02-18 18:25:29 +0200

    another solution

---
+++ Programming Basics/JS Basics - Exams/Programming Basics Online Pre - Exam - 17 and 18 February 2024/05. Hair Salon.js
@@ -0,0 +1,18 @@
+function hairSalon(input) {
+    let [countSea, countMountain, total, package, index] = [Number(input[0]), Number(input[1]), 0, input[2], 3]
+    while (package !== "Stop" && (countSea > 0 || countMountain > 0)) {
+        if (package === "sea" && countSea > 0) {
+            total += 680
+            countSea--
+        } else if (package === "mountain" && countMountain > 0) {
+            total += 499
+            countMountain--
+        }
+        package = input[index]
+        index++
+    }
+    if (countSea + countMountain === 0) {
+        console.log(`Good job! Everything is sold.`)
+    }
+    console.log(`Profit: ${total} leva.`)
+}
