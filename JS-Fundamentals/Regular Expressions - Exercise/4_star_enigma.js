8aa8735b68caaf05ed2eb11b6285bea068124a76
ceo-py <sfmf@abv.bg>
2024-02-19 12:43:13 +0200

    another solution

--- JS-Fundamentals/Regular Expressions - Exercise/4_star_enigma.js
+++ JS-Fundamentals/Regular Expressions - Exercise/4_star_enigma.js
@@ -28,3 +28,46 @@ function starEnigma(messages) {
 }
 
 starEnigma(['2', 'STCDoghudd4=63333$D$0A53333', 'EHfsytsnhf?8555&I&2C9555SR'])
+
+
+// function starEnigma(messages) {
+//     let planets = {attacked: [], destroyed: []};
+//     messages.shift();
+//
+//     messages.forEach(line => {
+//         const pattern = /@(?<planetName>[A-Za-z]+)[^@\-!:>]*?:(?<planetPopulation>\d+)[^@\-!:>]*?!(?<attackType>[AD])![^@\-!:>]*?->(?<soldierCount>\d+)/g;
+//
+//         let encryptionKey = 0;
+//         if (line.match(/[star]/gi) !== null) {
+//             encryptionKey = line.match(/[star]/gi).length;
+//         }
+//         line = line.split('').map(letter => letter.charCodeAt(0) - encryptionKey);
+//         line = line.map(code => String.fromCharCode(code)).join('');
+//
+//         const match = pattern.exec(line);
+//
+//         if (match !== null) {
+//             const attackType = match.groups.attackType;
+//             const planetName = match.groups.planetName;
+//
+//             switch (attackType) {
+//                 case 'A':
+//                     planets.attacked.push(planetName);
+//                     break;
+//                 case 'D':
+//                     planets.destroyed.push(planetName);
+//                     break;
+//             }
+//         }
+//
+//     })
+//     planets.attacked.sort((a, b) => a.localeCompare(b));
+//     planets.destroyed.sort((a, b) => a.localeCompare(b));
+//
+//     console.log(`Attacked planets: ${planets.attacked.length}`);
+//     planets.attacked.forEach(planet => console.log(`-> ${planet}`));
+//
+//     console.log(`Destroyed planets: ${planets.destroyed.length}`);
+//     planets.destroyed.forEach(planet => console.log(`-> ${planet}`));
+//
+// }
