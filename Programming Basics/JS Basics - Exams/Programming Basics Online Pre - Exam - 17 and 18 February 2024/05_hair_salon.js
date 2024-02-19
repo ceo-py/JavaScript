3eeb116b455ffb6c4237f37f2f88b248cc7c741d
ceo-py <sfmf@abv.bg>
2024-02-20 01:06:23 +0200

    another solution

--- Programming Basics/JS Basics - Exams/Programming Basics Online Pre - Exam - 17 and 18 February 2024/05_hair_salon.js
+++ Programming Basics/JS Basics - Exams/Programming Basics Online Pre - Exam - 17 and 18 February 2024/05_hair_salon.js
@@ -1,33 +1,54 @@
 function hairSalon(input) {
-    let [sumTarget, total, isTargetReach] = [Number(input[0]), 0, false]
-    for (let index = 1; index < input.length; index += 2) {
-        const service = input[index]
-        if (service === "closed") break
+    let [sumTarget, total, service, index] = [Number(input[0]), 0, input[1], 2]
+
+    while (service !== "closed" && !total >= sumTarget) {
         let kindService = input[index + 1]
-        switch (kindService) {
-            case "mens":
-                total += 15
-                break;
-            case "ladies":
-                total += 20
-                break;
-            case "kids":
-                total += 10
-                break;
-            case "touch up":
-                total += 20
-                break;
-            case "full color":
-                total += 30
-                break;
-        }
-        if (total >= sumTarget) {
-            isTargetReach = true;
-            break;
-        }
+        if (kindService === "mens") total += 15
+        else if (kindService === "ladies") total += 20
+        else if (kindService === "kids") total += 10
+        else if (kindService === "touch up") total += 20
+        else if (kindService === "full color") total += 30
+        index += 2
+        service = input[index]
     }
-    if (isTargetReach) {
-        console.log(`You have reached your target for the day!`)
-    } else console.log(`Target not reached! You need ${sumTarget - total}lv. more.`)
+    console.log(total >= sumTarget ?
+        `You have reached your target for the day!`
+        : `Target not reached! You need ${sumTarget - total}lv. more.`)
     console.log(`Earned money: ${total}lv.`)
 }
+
+
+//
+// function hairSalon(input) {
+//     let [sumTarget, total, isTargetReach] = [Number(input[0]), 0, false]
+//     for (let index = 1; index < input.length; index += 2) {
+//         const service = input[index]
+//         if (service === "closed") break
+//         let kindService = input[index + 1]
+//         switch (kindService) {
+//             case "mens":
+//                 total += 15
+//                 break;
+//             case "ladies":
+//                 total += 20
+//                 break;
+//             case "kids":
+//                 total += 10
+//                 break;
+//             case "touch up":
+//                 total += 20
+//                 break;
+//             case "full color":
+//                 total += 30
+//                 break;
+//         }
+//         if (total >= sumTarget) {
+//             isTargetReach = true;
+//             break;
+//         }
+//     }
+//     if (isTargetReach) {
+//         console.log(`You have reached your target for the day!`)
+//     } else console.log(`Target not reached! You need ${sumTarget - total}lv. more.`)
+//     console.log(`Earned money: ${total}lv.`)
+// }
