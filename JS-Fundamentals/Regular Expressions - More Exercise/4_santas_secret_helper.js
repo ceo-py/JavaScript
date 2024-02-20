13414b6f20e8a16b9b6c26ca0621860c8bacb915
ceo-py <sfmf@abv.bg>
2024-02-20 22:04:41 +0200

    another solution

---
+++ JS-Fundamentals/Regular Expressions - More Exercise/4_santas_secret_helper.js
@@ -0,0 +1,23 @@
+function santaSecretHelper(input) {
+    const [key, goodChildren] = [input.shift(), []]
+
+    function GetDecodedInput(input, key) {
+        return input.split('').map(x => String.fromCharCode(x.charCodeAt(0) - key)).join('')
+    }
+
+    for (const info of input) {
+        if (info === 'end') break
+
+        const data = GetDecodedInput(info, key);
+        let [regex, matchInfo] = [/[@](?<name>[A-Za-z]+)[^@!:>-]+[!](?<behaviour>[G|N])[!]/g, '']
+
+        while ((matchInfo = regex.exec(data)) !== null) {
+            let [childName, childBehaviour] = [matchInfo.groups.name, matchInfo.groups.behaviour]
+            if (childBehaviour === 'G') goodChildren.push(childName)
+        }
+    }
+    console.log(goodChildren.join('\n'))
+}
+
+
+santaSecretHelper(['3', 'CNdwhamigyenumje$J$', 'CEreelh-nmguuejnW$J$', 'CVwdq&gnmjkvng$Q$', 'end'])
