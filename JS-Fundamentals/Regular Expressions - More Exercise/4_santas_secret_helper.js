cc592eb0a7115dd17500e3b1a569c1edc7b98750
ceo-py <sfmf@abv.bg>
2024-02-20 22:24:14 +0200

    another solution

--- JS-Fundamentals/Regular Expressions - More Exercise/4_santas_secret_helper.js
+++ JS-Fundamentals/Regular Expressions - More Exercise/4_santas_secret_helper.js
@@ -1,15 +1,12 @@
 function santaSecretHelper(input) {
     const [key, goodChildren] = [input.shift(), []]
 
-    for (const info of input) {
-        if (info === 'end') break
-
+    for (const info of input.splice(0, input.indexOf('end'))) {
         const data = info.split('').map(x => String.fromCharCode(x.charCodeAt(0) - key)).join('')
         let [regex, matchInfo] = [/[@](?<name>[A-Za-z]+)[^@!:>-]+[!](?<behaviour>[G|N])[!]/g, '']
 
         while ((matchInfo = regex.exec(data)) !== null) {
-            const { name: childName, behaviour: childBehaviour } = matchInfo.groups
-            // const [childName, childBehaviour] = [matchInfo.groups.name, matchInfo.groups.behaviour]
+            const {name: childName, behaviour: childBehaviour} = matchInfo.groups
             if (childBehaviour === 'G') goodChildren.push(childName)
         }
     }
@@ -18,3 +15,21 @@ function santaSecretHelper(input) {
 
 
 santaSecretHelper(['3', 'CNdwhamigyenumje$J$', 'CEreelh-nmguuejnW$J$', 'CVwdq&gnmjkvng$Q$', 'end'])
+
+
+// function santaSecretHelper(input) {
+//     const [key, goodChildren] = [input.shift(), []]
+//
+//     for (const info of input) {
+//         if (info === 'end') break
+//
+//         const data = info.split('').map(x => String.fromCharCode(x.charCodeAt(0) - key)).join('')
+//         let [regex, matchInfo] = [/[@](?<name>[A-Za-z]+)[^@!:>-]+[!](?<behaviour>[G|N])[!]/g, '']
+//
+//         while ((matchInfo = regex.exec(data)) !== null) {
+//             const [childName, childBehaviour] = [matchInfo.groups.name, matchInfo.groups.behaviour]
+//             if (childBehaviour === 'G') goodChildren.push(childName)
+//         }
+//     }
+//     console.log(goodChildren.join('\n'))
+// }
