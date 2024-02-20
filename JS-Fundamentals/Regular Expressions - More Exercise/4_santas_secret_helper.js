087020d1a834b0a95ec3588dbdddcb28e4e26329
ceo-py <sfmf@abv.bg>
2024-02-20 22:10:25 +0200

    another solution

--- JS-Fundamentals/Regular Expressions - More Exercise/4_santas_secret_helper.js
+++ JS-Fundamentals/Regular Expressions - More Exercise/4_santas_secret_helper.js
@@ -12,7 +12,8 @@ function santaSecretHelper(input) {
         let [regex, matchInfo] = [/[@](?<name>[A-Za-z]+)[^@!:>-]+[!](?<behaviour>[G|N])[!]/g, '']
 
         while ((matchInfo = regex.exec(data)) !== null) {
-            let [childName, childBehaviour] = [matchInfo.groups.name, matchInfo.groups.behaviour]
+            const { name: childName, behaviour: childBehaviour } = matchInfo.groups
+            // const [childName, childBehaviour] = [matchInfo.groups.name, matchInfo.groups.behaviour]
             if (childBehaviour === 'G') goodChildren.push(childName)
         }
     }
