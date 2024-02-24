f8a197240b7c1e2fb478aec6224df099f9ae8d98
ceo-py <sfmf@abv.bg>
2024-02-24 20:03:24 +0200

    another solution

--- JS-Fundamentals/Associative Arrays - Exercise/9_arena_tier.js
+++ JS-Fundamentals/Associative Arrays - Exercise/9_arena_tier.js
@@ -38,3 +38,48 @@ function arenaTier(list) {
         })
     })
 }
+
+
+
+
+// function arenaTier(list) {
+//     let gladiatorsList = {};
+//
+//     list.forEach(x => {
+//         if (x.includes('->')) {
+//             const [name, skill, amount] = x.split(' -> ').map(x => isNaN(x) ? x : Number(x));
+//             gladiatorsList[name] = gladiatorsList[name] || {};
+//             gladiatorsList[name][skill] = Math.max(amount, gladiatorsList[name][skill] || 0);
+//         } else if (x.includes(' vs ')) {
+//             const [gladiatorOneName, gladiatorTwoName] = x.split(' vs ');
+//             if (gladiatorsList[gladiatorOneName] && gladiatorsList[gladiatorTwoName]) {
+//                 const foundSkills = Object.keys(gladiatorsList[gladiatorOneName])
+//                     .filter(skill => gladiatorsList[gladiatorTwoName].hasOwnProperty(skill));
+//                 if (foundSkills.length !== 0) {
+//                     const gladiatorOnePower = Object.values(gladiatorsList[gladiatorOneName]).reduce((acc, val) => acc + val, 0);
+//                     const gladiatorTwoPower = Object.values(gladiatorsList[gladiatorTwoName]).reduce((acc, val) => acc + val, 0);
+//                     if (gladiatorOnePower > gladiatorTwoPower) {
+//                         delete gladiatorsList[gladiatorTwoName];
+//                     } else {
+//                         delete gladiatorsList[gladiatorOneName];
+//                     }
+//                 }
+//             }
+//         }
+//     });
+//
+//     const sortedGladiators = Object.entries(gladiatorsList).sort(([aKey, aValue], [bKey, bValue]) => {
+//         const aSum = Object.values(aValue).reduce((acc, val) => acc + val, 0);
+//         const bSum = Object.values(bValue).reduce((acc, val) => acc + val, 0);
+//         return bSum - aSum;
+//     });
+//
+//     sortedGladiators.forEach(([gladiator, skills]) => {
+//         console.log(`${gladiator}: ${Object.values(skills).reduce((acc, skill) => acc + skill, 0)} skill`);
+//         const sortedSkills = Object.entries(skills)
+//             .sort(([aSkill, aPoints], [bSkill, bPoints]) => bPoints - aPoints || aSkill.localeCompare(bSkill));
+//         sortedSkills.forEach(([skill, points]) => {
+//             console.log(`- ${skill} <!> ${points}`);
+//         });
+//     });
+// }
