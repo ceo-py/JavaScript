b020b779ec8c5845e8241606ed41efa91df62be0
ceo-py <sfmf@abv.bg>
2024-03-03 13:45:23 +0200

    another solution

--- JS-Fundamentals/Fundamentals-Exam/02. Programming Fundamentals Mid Exam/1_softuni_reception.js
+++ JS-Fundamentals/Fundamentals-Exam/02. Programming Fundamentals Mid Exam/1_softuni_reception.js
@@ -1,7 +1,7 @@
 function softUniReception(employee_info) {
     employee_info = employee_info.map(Number)
-    let [total_answers_per_hour, students_in_q, time_needed] = [employee_info.slice(0, 3).reduce((a, b) => a + b, 0), employee_info[3], 0];
-
+    let [total_answers_per_hour, students_in_q, time_needed] = [eval(employee_info.slice(0, 3).join('+')), employee_info[3], 0];
+    // let [total_answers_per_hour, students_in_q, time_needed] = [employee_info.slice(0, 3).reduce((a, b) => a + b, 0), employee_info[3], 0];
     while (students_in_q > 0) {
         time_needed++;
         if (time_needed % 4 !== 0) {
