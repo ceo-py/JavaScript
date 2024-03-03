03740a615b190d72cb93a6fa747f0bbb2667de59
ceo-py <sfmf@abv.bg>
2024-03-03 13:44:05 +0200

    another solution

---
+++ JS-Fundamentals/Fundamentals-Exam/02. Programming Fundamentals Mid Exam/1_softuni_reception.js
@@ -0,0 +1,15 @@
+function softUniReception(employee_info) {
+    employee_info = employee_info.map(Number)
+    let [total_answers_per_hour, students_in_q, time_needed] = [employee_info.slice(0, 3).reduce((a, b) => a + b, 0), employee_info[3], 0];
+
+    while (students_in_q > 0) {
+        time_needed++;
+        if (time_needed % 4 !== 0) {
+            students_in_q -= total_answers_per_hour;
+        }
+    }
+
+    console.log(`Time needed: ${time_needed}h.`);
+}
+
+softUniReception(['1','2','3','45'])
