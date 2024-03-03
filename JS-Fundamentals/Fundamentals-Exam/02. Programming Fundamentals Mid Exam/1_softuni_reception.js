a179f9e7f0ee83dac841d533d132304bdfb46599
ceo-py <sfmf@abv.bg>
2024-03-03 13:58:33 +0200

    another solution

--- JS-Fundamentals/Fundamentals-Exam/02. Programming Fundamentals Mid Exam/1_softuni_reception.js
+++ JS-Fundamentals/Fundamentals-Exam/02. Programming Fundamentals Mid Exam/1_softuni_reception.js
@@ -1,15 +1,14 @@
-function softUniReception(employee_info) {
-    employee_info = employee_info.map(Number)
-    let [total_answers_per_hour, students_in_q, time_needed] = [eval(employee_info.slice(0, 3).join('+')), employee_info[3], 0];
-    // let [total_answers_per_hour, students_in_q, time_needed] = [employee_info.slice(0, 3).reduce((a, b) => a + b, 0), employee_info[3], 0];
-    while (students_in_q > 0) {
-        time_needed++;
-        if (time_needed % 4 !== 0) {
-            students_in_q -= total_answers_per_hour;
+function softUniReception(employeeInfo) {
+    employeeInfo = employeeInfo.map(Number);
+    let [studentsInQueue, totalAnswersPerHour, , timeNeeded] = [employeeInfo.pop(), eval(employeeInfo.join('+')), employeeInfo[3], 0];
+    // let [totalAnswersPerHour, studentsInQueue, timeNeeded] = [employeeInfo.slice(0, 3).reduce((a, b) => a + b, 0), employeeInfo[3], 0];
+    while (studentsInQueue > 0) {
+        timeNeeded++;
+        if (timeNeeded % 4 !== 0) {
+            studentsInQueue -= totalAnswersPerHour;
         }
     }
-
-    console.log(`Time needed: ${time_needed}h.`);
+    console.log(`Time needed: ${timeNeeded}h.`);
 }
 
-softUniReception(['1','2','3','45'])
+softUniReception(['1','2','3','45']);
