ff66b1d39ecd8ef6a909feda0bb7dc3ffeb5bfc6
ceo-py <sfmf@abv.bg>
2024-03-06 12:10:29 +0200

    another solution

---
+++ JS Advanced/JS Advanced - Exercises/Syntax, Functions and Statements - Lab/9_square_of_stars.js
@@ -0,0 +1,8 @@
+function squareOfStars(input) {
+    const number = Number(input)
+    const toPrint = "* ".repeat(number);
+
+    for (let row = 0; row < number; row++) {
+        console.log(toPrint);
+    }
+}
