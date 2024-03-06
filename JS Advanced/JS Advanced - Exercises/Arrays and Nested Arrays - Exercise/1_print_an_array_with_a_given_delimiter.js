febe1ff80efb3a55d30c68c85cf67a90bda2b07f
ceo-py <sfmf@abv.bg>
2024-03-06 14:20:54 +0200

    another solution

---
+++ JS Advanced/JS Advanced - Exercises/Arrays and Nested Arrays - Exercise/1_print_an_array_with_a_given_delimiter.js
@@ -0,0 +1,10 @@
+function printAnArrayWithAGivenDelimiter(input, separator) {
+    console.log(input.join(separator))
+}
+
+printAnArrayWithAGivenDelimiter(['One',
+'Two',
+'Three',
+'Four',
+'Five'],
+'-')
