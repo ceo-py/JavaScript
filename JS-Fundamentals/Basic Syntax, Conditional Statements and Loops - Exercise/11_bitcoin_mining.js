7249fd23b5734e5cad2cc77d435d33a38115c6d7
ceo-py <sfmf@abv.bg>
2024-03-07 11:20:22 +0200

    another solution

--- JS-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/11_bitcoin_mining.js
+++ JS-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/11_bitcoin_mining.js
@@ -19,5 +19,29 @@ function bitcoinMining(list) {
 }
 
 
-// bitcoinMining([3124.15, 504.212, 2511.124])
-bitcoinMining([100, 200, 300])
+// function bitcoinMining(array) {
+//     let [gold, currentBitcoin, count, day, totalBitcoin] = [0, 0, 0, 0, 0];
+//     for (let i = 0; i < array.length; i++) {
+//         count++;
+//         if ((i + 1) % 3 === 0) {
+//             gold += array[i] * 67.51 * 0.7;
+//         } else {
+//             gold += array[i] * 67.51;
+//         }
+//         if (gold >= 11949.16) {
+//             currentBitcoin = Math.floor(gold / 11949.16);
+//             gold = gold - 11949.16 * currentBitcoin;
+//             totalBitcoin += currentBitcoin;
+//             if (!day) {
+//                 day = count;
+//             }
+//         }
+//     }
+//     console.log(`Bought bitcoins: ${totalBitcoin}`);
+//     if (day) {
+//         console.log(`Day of the first purchased bitcoin: ${day}`);
+//     }
+//     console.log(`Left money: ${gold.toFixed(2)} lv.`);
+// }
+
+bitcoinMaining([3124.15, 504.212, 2511.124]);
