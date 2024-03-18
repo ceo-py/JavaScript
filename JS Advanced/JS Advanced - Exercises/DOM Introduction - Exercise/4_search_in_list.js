77b3bc35e57cef109bb6b214cf1e4c4fa10ac3b6
ceo-py <sfmf@abv.bg>
2024-03-18 21:11:34 +0200

    add solution

---
+++ JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/4_search_in_list.js
@@ -0,0 +1,18 @@
+function searchInList() {
+    const towns = Array.from(document.querySelectorAll('li'))
+    const result = document.querySelector('#result')
+    const searchingText = document.querySelector('#searchText').value
+
+    let matchedSearch = 0
+
+    towns.forEach(x => {
+        x.style.fontWeight = ''
+        x.style.textDecoration = ''
+        if (x.textContent.includes(searchingText)) {
+            matchedSearch += 1
+            x.style.fontWeight = 'bold'
+            x.style.textDecoration = 'underline'
+        }
+    })
+    result.textContent = `${matchedSearch} matches found`
+}
