a4ee8139eec2744aece165887d4c47e0c420085c
ceo-py <sfmf@abv.bg>
2024-03-18 21:13:10 +0200

    add solution

---
+++ JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/5_table_search_engine.js
@@ -0,0 +1,16 @@
+function tableSearchEngine() {
+    document.querySelector('#searchBtn').addEventListener('click', onClick)
+
+    function onClick() {
+        const table = [...document.querySelectorAll('tbody > tr')]
+        const searchField = document.querySelector('#searchField')
+
+        table.forEach(item => {
+            item.classList.remove('select')
+            if (item.innerText.includes(searchField.value) && searchField.value.length > 0) {
+                item.className = 'select'
+            }
+        })
+        searchField.value = ''
+    }
+}
