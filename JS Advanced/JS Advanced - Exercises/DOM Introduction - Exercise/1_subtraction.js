7671a86e8a412591266978a1068393c92b847542
ceo-py <sfmf@abv.bg>
2024-03-18 21:06:13 +0200

    add solution

---
+++ JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/1_subtraction.js
@@ -0,0 +1,3 @@
+function subtraction() {
+    document.querySelector('#result').textContent = parseFloat(document.querySelector('#firstNumber').value) - parseFloat(document.querySelector('#secondNumber').value);
+}
