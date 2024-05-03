ede4412dfde13ea579666705935d4005e380dde6
ceo-py <sfmf@abv.bg>
2024-05-03 21:21:47 +0300

    update solution

--- JS Advanced/JS Advanced - Exercises/Arrays and Nested Arrays - Exercise/10_tic_tac_toe.js
+++ JS Advanced/JS Advanced - Exercises/Arrays and Nested Arrays - Exercise/10_tic_tac_toe.js
@@ -1,58 +1,80 @@
 function ticTacToe(moves) {
-    let dashboard = [
-        [false, false, false],
-        [false, false, false],
-        [false, false, false]
-    ];
+  let board = Array.from({ length: 3 }, () => Array(3).fill(false));
+  const players = ["X", "O"];
+  let moveCount = 0;
 
-    let players = ['X', 'O'];
+  function printBoard(board) {
+    board.forEach((row) => {
+      console.log(row.join("\t"));
+    });
+  }
 
-    // moves = moves.filter((move, i) => {
-    //     let [row, col] = move.split(' ').map(Number);
-    //     if (dashboard[row][col] !== false) {
-    //         console.log("This place is already taken. Please choose another!");
-    //         return false;
-    //     }
-    //     dashboard[row][col] = players[i % 2];
-    //     return true;
-    // });
-
-    for (let i = 0; i < moves.length; i++) {
-        let [row, col] = moves[i].split(' ').map(Number);
-        let player = players[i % 2];
-        dashboard[row][col] = player;
-
-        for (let j = 0; j < 3; j++) {
-            if (
-                dashboard[j][0] === player && dashboard[j][1] === player && dashboard[j][2] === player ||
-                dashboard[0][j] === player && dashboard[1][j] === player && dashboard[2][j] === player
-            ) {
-                console.log(`Player ${player} wins!`);
-                console.log(dashboard.map(row => row.join('\t')).join('\n'));
-                return;
-            }
-        }
-
-        if (dashboard[0][0] === player && dashboard[1][1] === player && dashboard[2][2] === player ||
-            dashboard[0][2] === player && dashboard[1][1] === player && dashboard[2][0] === player
-        ) {
-            console.log(`Player ${player} wins!`);
-            console.log(dashboard.map(row => row.join('\t')).join('\n'));
-            return;
-        }
+  function checkWinner(board, player) {
+    for (let i = 0; i < 3; i++) {
+      if (board[i].every((cell) => cell === player)) {
+        return true;
+      }
+      if (board.map((row) => row[i]).every((cell) => cell === player)) {
+        return true;
+      }
     }
 
-    console.log("The game ended! Nobody wins :(");
-    console.log(dashboard.map(row => row.join('\t')).join('\n'));
+    if (
+      board[0][0] === player &&
+      board[1][1] === player &&
+      board[2][2] === player
+    ) {
+      return true;
+    }
+    if (
+      board[0][2] === player &&
+      board[1][1] === player &&
+      board[2][0] === player
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  for (let move of moves) {
+    let [row, col] = move.split(" ").map(Number);
+
+    if (board[row][col]) {
+      console.log("This place is already taken. Please choose another!");
+      continue;
+    }
+
+    let currentPlayer = players[moveCount % 2];
+    board[row][col] = currentPlayer;
+    moveCount++;
+
+    if (checkWinner(board, currentPlayer)) {
+      console.log(`Player ${currentPlayer} wins!`);
+      printBoard(board);
+      return;
+    }
+
+    if (moveCount === 9) {
+      console.log("The game ended! Nobody wins :(");
+      printBoard(board);
+      return;
+    }
+  }
+
+  console.log("The game ended! Nobody wins :(");
+  printBoard(board);
 }
 
-ticTacToe(["0 1",
- "0 0",
- "0 2",
- "2 0",
- "1 0",
- "1 2",
- "1 1",
- "2 1",
- "2 2",
- "0 0"])
+ticTacToe([
+  "0 1",
+  "0 0",
+  "0 2",
+  "2 0",
+  "1 0",
+  "1 1",
+  "1 2",
+  "2 2",
+  "2 1",
+  "0 0",
+]);
