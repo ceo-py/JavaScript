ea58f576dd975ce54a29da7f29c1a9016b0aba45
ceo-py <sfmf@abv.bg>
2024-10-13 21:48:20 +0300

    fixed change input on task from exam

--- Programming Basics/JS Basics - Exams/Programming Basics Online Exam - 9 and 10 March 2019/2_football_results.js
+++ Programming Basics/JS Basics - Exams/Programming Basics Online Exam - 9 and 10 March 2019/2_football_results.js
@@ -1,4 +1,4 @@
-function footballResults(input) {
+function footballResults(...input) {
     let [wins, loses, draws] = [0, 0, 0]
     for (const score of input) {
         const [homeTeam, awayTeam] = score.split(':').map(Number)
@@ -12,3 +12,16 @@ function footballResults(input) {
 }
 
 footballResults(['3:1', '0:2', '0:0'])
+
+// function footballResults(gameOne, gameTwo, gameThree) {
+//     let [wins, loses, draws] = [0, 0, 0]
+//     for (const score of [gameOne, gameTwo, gameThree]) {
+//         const [homeTeam, awayTeam] = score.split(':').map(Number)
+//         if (homeTeam > awayTeam) wins ++
+//         else if (homeTeam < awayTeam) loses ++
+//         else if (homeTeam === awayTeam) draws ++
+//     }
+//     console.log(`Team won ${wins} games.`)
+//     console.log(`Team lost ${loses} games.`)
+//     console.log(`Drawn games: ${draws}`)
+// }
